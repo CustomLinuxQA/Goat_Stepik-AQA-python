f41bcd4d5fdf373ff46165b921e8af7ae83d3004
custom_linux <customlinuxqa@gmail.com>
2022-12-06 18:39:56 +0300

    add file

--- section1/lesson6_step3_ Working-with-the-browser-in-Selenium.py
+++ section1/lesson6_step3_ Working-with-the-browser-in-Selenium.py
@@ -6,10 +6,13 @@ chromedriver_autoinstaller.install()
 
 
 link = "http://suninjuly.github.io/simple_form_find_task.html"
-browser = webdriver.Chrome()
-browser.get(link)
-button = browser.find_element(By.ID, "submit_button")
-button.click()
 
-# закрываем браузер после всех манипуляций
-browser.quit()
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    button = browser.find_element(By.ID, "submit_button")
+    button.click()
+
+finally:
+    # закрываем браузер после всех манипуляций
+    browser.quit()
